d691a5c8db4fa2e0be5a7cf7c3359dd03700dc49
huangruih <huangruih@caifu500.com>
2017-08-30 17:03:58 +0800

    update

--- service0/src/main/java/com/sample/service0/HelloController.java
+++ service0/src/main/java/com/sample/service0/HelloController.java
@@ -1,5 +1,7 @@
 package com.sample.service0;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 //import org.springframework.cloud.client.serviceregistry.Registration;
 @RestController
+@RequestMapping("/api")
 public class HelloController {
     protected static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @Autowired
     private DiscoveryClient client;
 
+    @ApiOperation(value="获取用户列表", notes="dddddddd")
     @RequestMapping("/hello")
     public String index () {
 
